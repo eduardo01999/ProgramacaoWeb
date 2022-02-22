a6a4e731d6f04772b0ae9c0cef611b9b3179ad04
Eduardo Frésca <eduardomfresca@gmail.com>
2022-02-22 20:44:48 -0300

    Começando exercicio 1

---
+++ primeiroprojeto/resources/views/exercicio1.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('titulo')
+    Primeiro Exercicio
+@endsection
+
+@section('conteudo')
+    <p> No sábado eu vou fazer esse exercicio</p>
+@endsection
--- primeiroprojeto/routes/web.php
+++ primeiroprojeto/routes/web.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/index', [HomeController::class, 'index' ]);
+
+Route::get('/primeiroexercicio', [HomeController::class, 'primeiroexercicio']);
