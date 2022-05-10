2bead20b8539ba643961f440eeda4fce8922b871
Eduardo Frésca <eduardomfresca@gmail.com>
2022-05-10 19:37:40 -0300

    Alterando front das index de categoria e fornecedor, deixando a tabela mais apresentavel

--- ecommerce/resources/views/fornecedor/index.blade.php
+++ ecommerce/resources/views/fornecedor/index.blade.php
@@ -7,50 +7,48 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                    <div class="flex items-center mt-4 mb-10">
-                        <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3" href="{{ route('fornecedor.create')}}" >Novo Registro</a>
-                    </div>
-                    <div class="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard px-8 pt-3
-                                rounded-bl-lg rounded-br-lg mt-8 ">
-                        <table class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                            <thead>
-                            <tr>
-                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-lg leading-4 text-black-500
-                                            tracking-wider">Fornecedor</th>
-                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-lg leading-4 text-black-500
-                                            tracking-wider"></th>
-                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-lg leading-4 text-black-500
-                                            tracking-wider"></th>
-                            </tr>
-                            </thead>
+                <div class="flex items-center mt-4 mb-10">
+                    <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3" href="{{ route('fornecedor.create')}}" >Novo Fornecedor</a>
+                </div>
 
-                            <tbody>
-                            @foreach($fornecedor as $f)
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b text-gray-900 border-gray-500 text-sm
-                                            leading-5">{{$f->nomeFantasia}}</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-sm leading-5 text-right ">
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b text-gray-900 border-gray-500 text-sm
-                                            leading-5">{{$f->numTel}}</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-sm leading-5 text-right ">
-                                        <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3" href="{{ route('fornecedor.edit', $f->id)}}" >
-                                            Alterar
-                                        </a>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-sm leading-5 text-right ">
-                                        <form action="{{route('fornecedor.destroy', $f->id)}}" method="POST">
-                                            @csrf
-                                            @method("DELETE")
-                                            <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3" >
-                                                Excluir
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Nome
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Telefone
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($fornecedor as $f)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                    {{$f->nomeFantasia}}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{$f->numTel}}
+                                </td>
+                                <td class="px-6 py-4 text-right">
+                                    <a href="{{route("fornecedor.edit", $f->id)}}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Alterar</a>
+                                    <form action="{{route('fornecedor.destroy', $f->id)}}" method="POST">
+                                        @csrf
+                                        @method("DELETE")
+                                        <button class="font-medium text-blue-600 dark:text-blue-500 hover:underline" >
+                                            Excluir
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
